cb21bad887d2f29bb837a6f965f96339b56655fc
Dmitriy Pochodnya <dmitriy.troy@gmail.com>
2017-04-07 11:40:54 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+export default {
+  components: {
+    appHeader: Header
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="nav has-shadow">
+    <div class="nav-left">
+      <router-link class="nav-item" to="/">Stock Trader</router-link>
+      <router-link class="nav-item is-tab" activeClass="is-active" to="/portfolio">Portfolio</router-link>
+      <router-link class="nav-item is-tab" activeClass="is-active" to="/stocks">Stocks</router-link>
+    </div>
+    <div class="nav-right nav-menu">
+      <a href="#" class="nav-item is-tab">End Day</a>
+      <div class="has-dropdown">
+        <a href="#" class="button is-medium">Save & Load</a>
+        <ul class="dropdown box">
+          <li>
+            <a href="#">Save Data</a>
+          </li>
+          <li>
+            <a href="#">Load Data</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
